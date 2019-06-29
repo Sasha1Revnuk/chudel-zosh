a51435ed37bf46741b6ec8c9191a4f6320307de9
Sasha1Revnuk <revo0708@gmail.com>
2019-06-29 21:06:53 +0300

    smal fix

--- app/Http/Controllers/SymbolismController.php
+++ app/Http/Controllers/SymbolismController.php
@@ -8,8 +8,10 @@ use Illuminate\Http\Request;
 
 class SymbolismController extends Controller
 {
+
     public function index(Request $request)
     {
+        $banner = Menu::where('src', $request->server->get('PATH_INFO'))->first();
         $data = [
             'settings' => $this->getSettings(),
             'menu' => $this->getMenu(),
@@ -17,7 +19,7 @@ class SymbolismController extends Controller
             'user' => $this->user,
             'role' => $this->role,
             'title' => 'Візитка школи',
-            'banner' => Menu::where('src', $request->server->get('PATH_INFO'))->first()->banner,
+            'banner' => $banner->banner != 0 ? $banner->banner : null,
             'symbolism' => Symbolism::first()
         ];
         return view ('symbolism')->with(['data' => $data]);
